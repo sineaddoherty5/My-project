d4d562335d3031ee4771a96f22aef7c73a64fc1f
sineaddoherty5 <sinead2.doherty@live.uwe.ac.uk>
2024-03-22 21:25:45 +0000

    panel appear

--- Assets/scripts/PanelOne.cs
+++ Assets/scripts/PanelOne.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class PanelOne : MonoBehaviour
 {
+
+    public GameObject ui;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        HideUI();
     }
 
     // Update is called once per frame
@@ -15,4 +18,25 @@ public class PanelOne : MonoBehaviour
     {
         
     }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            ShowUI();
+        }
+    }
+
+
+    public void HideUI()
+    {
+        ui.SetActive(false); 
+    }
+
+    public void ShowUI()
+    {
+        ui.SetActive(true);
+    }
+
 }
+
